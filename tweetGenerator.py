729c3085a9eb838080e87c464ff69908a7d8d5a3
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2022-12-27 12:42:35 +0530

    Update tweetGenerator.py

--- tweetGenerator.py
+++ tweetGenerator.py
@@ -38,7 +38,7 @@ def get_file_content_chrome(driver, uri):
 def generate(iname, iusername,itweet,itweet_image):
   
 
-    driver = webdriver.Chrome('chromedriver',chrome_options=options)
+    driver = webdriver.Chrome('\chromedriver',chrome_options=options)
     #driver = get_driver()
     
     driver.get(os.getcwd()+"\webpage\Tweetgen.html")
