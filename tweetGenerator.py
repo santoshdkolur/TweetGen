6f2f0b64281d6c862d587c6929833e328bfa5681
Santosh Kolur <48786464+santoshdkolur@users.noreply.github.com>
2022-12-27 12:56:50 +0530

    Update tweetGenerator.py

--- tweetGenerator.py
+++ tweetGenerator.py
@@ -15,7 +15,7 @@ options = Options()
 options.add_argument("headless")
 
 def get_driver():
-  return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+  return webdriver.Chrome(service=Service('/usr/local/bin/chromedriver'), options=options)
 
 
 
@@ -38,8 +38,8 @@ def get_file_content_chrome(driver, uri):
 def generate(iname, iusername,itweet,itweet_image):
   
 
-    driver = webdriver.Chrome('/app/tweetgen/chromedriver',chrome_options=options)
-    #driver = get_driver()
+    #driver = webdriver.Chrome('/app/tweetgen/chromedriver',chrome_options=options)
+    driver = get_driver()
     
     driver.get(os.getcwd()+"\webpage\Tweetgen.html")
     theme = driver.find_element(by=By.XPATH,value="/html/body/div/div/div[1]/form/div[2]/div[3]/label")
